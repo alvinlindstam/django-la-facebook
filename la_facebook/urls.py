557a54279bb72910834b4851baac94831efbd2c5
Emil Stenström <em@kth.se>
2015-05-25 13:52:22 +0200

    Remove unusual url call with normal one.

--- la_facebook/urls.py
+++ la_facebook/urls.py
@@ -2,14 +2,6 @@ from django.conf.urls.defaults import *
 
 
 urlpatterns = patterns("la_facebook.views",
-    url(
-        regex = r"^login/?$",
-        view = "facebook_login",
-        name = "la_facebook_login",
-    ),
-    url(
-        regex = r"^callback/?$",
-        view = "facebook_callback",
-        name = "la_facebook_callback"
-    ),
+    url(r"^login/?$", "facebook_login", name="la_facebook_login"),
+    url(r"^callback/?$", "facebook_callback", name="la_facebook_callback"),
 )
