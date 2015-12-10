424c4bcdbdfcf6d42dba3419c02ea8818d19da7d
Alvin Lindstam <alvin@kundo.se>
2015-12-10 10:13:50 +0100

    Remove length constraint on token field.
    They may be any length: https://stackoverflow.com/questions/4408945/what-is-the-length-of-the-access-token-in-facebook-oauth2/16365828#16365828

--- la_facebook/models.py
+++ la_facebook/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class UserAssociation(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, unique=True)
     identifier = models.CharField(max_length=255, db_index=True)
-    token = models.CharField(max_length=200)
+    token = models.TextField()
     expires = models.DateTimeField(null=True)
 
     def expired(self):
